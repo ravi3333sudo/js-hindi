f616b5ba9d14d225582f5e807e5bf2e863264e51
ravi3333sudo <raviraushan1313@gmail.com>
2024-07-28 13:10:55 +0000

    conversion Operation

---
+++ 01_basic/03conversionoperation.js
@@ -0,0 +1,64 @@
+//let score = 33
+
+//console.log(typeof score);
+//console.log(typeof(score));
+
+//let valueInNumber =Number(score)
+//console.log(typeof valueInNumber);
+//console.log(valueInNumber);
+
+//"33"=>33
+//"33abc" =>NaN
+// true=> 1; flase=>0
+
+//let isLoggedIn =""
+
+//let booleanIsLoggedIn = Boolean(isLoggedIn)
+//console.log(booleanIsLoggedIn);
+//1=>true; 0=> flase
+//""=>false
+//"ravi"=>true
+
+let someNumber =33
+
+let stringNumber =String(someNumber)
+//console.log(stringNumber);
+//console.log(typeof stringNumber);
+//****************** operations *************//
+
+let value = 3
+let negValue = -value
+//console.log(negValue);
+
+//console.log(2+2);
+//console.log(2-2);
+//console.log(2*2);
+//console.log(2**2);
+//console.log(2/3);
+//console.log(2%3);
+
+let str1 ="hello"
+let str2 ="ravi"
+
+let str3 =str1 + str2
+// console.log(str3);
+
+// console.log("1"+2);
+// console.log(1 + "2");
+// console.log("1"+2+2);
+// console.log(1+2+"2");
+// console.log( (3 + 4) * 5 % 3 );
+
+// console.log(+true);
+// console.log(+"");
+
+let num1,num2,num3
+num1 = num2 = num3 = 2 + 2
+
+let gameCounter = 100;
+gameCounter++;
+console.log(gameCounter);
+
+//link to study 
+// https://262.ecma-international.org/14.0/index.html?_gl=1*1kvbr7c*_ga*MTc2MjA1NDU3Ny4xNzIyMTcxMjM1*_ga_TDCK4DWEPP*MTcyMjE3MTIzNC4xLjEuMTcyMjE3MTI0NS4wLjAuMA..#sec-abstract-operations
+
