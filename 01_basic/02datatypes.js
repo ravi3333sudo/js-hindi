519b76a2cfb0ca94b5710630392c1ae4b1f8c7b7
ravi3333sudo <raviraushan1313@gmail.com>
2024-07-28 11:44:48 +0000

    datatypes

---
+++ 01_basic/02datatypes.js
@@ -0,0 +1,28 @@
+"use strict";//treat all JS code as newer version
+
+//alert(3+3)//we are using nodejs,not browser
+
+console.log[3+3] //code realibility is high
+
+console.log['Ravi']
+
+
+let name = "ravi"
+let age = 18
+let isLoggedIn = false
+let state;
+
+//number => 2 to power 53
+//bigint
+//string => ""
+//boolean=>true/false
+//null =>standalone value
+// undefined =>
+    //symbol => unique
+
+//object
+
+console.log(typeof "ravi")
+console.log(typeof age);
+console.log(typeof null);
+console.log(typeof undefined);
