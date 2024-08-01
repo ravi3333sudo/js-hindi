59113fd1b66e27667e37c1493401182a174018ce
ravi3333sudo <raviraushan1313@gmail.com>
2024-08-01 06:28:54 +0000

    datatypes summary

---
+++ 01_basic/05datatypes-summary.js
@@ -0,0 +1,31 @@
+//primitive
+
+// 7 types : string , Number ,Boolean ,null ,undefined , sybol , BigINt
+
+const score = 100
+const scoreValue =100.3
+
+const isLoggedIN = false
+const outsideTemp =null
+let userEmail;
+
+const id = Symbol ('123')
+const anotherId = Symbol('123')
+console.log(id === anotherId);
+
+//const bigNumber = 343456789877654n
+// Reference ( non primitive)
+
+// Array , Objects , Functions
+
+const heros = ["shaktiman", "naagraj" ,"doga"];
+let myObj ={
+    name: "ravi",
+    age: 22,
+}
+
+ const myFunction = function(){
+    console.log("HEllo world");
+ }
+
+ console.log(typeof bigNumber);
